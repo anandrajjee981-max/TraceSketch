3c9557edf5ddb6e82338a7baa8ccdf01ec7ba903
anand raj <git add README.md>
2026-08-31 23:13:54 +0530

    add tracedao

--- packages/collector/src/dao/trace.dao.ts
+++ packages/collector/src/dao/trace.dao.ts
@@ -1,13 +1,30 @@
-import { initSchema } from "../config/db";
+import { log } from "console";
+import { db, initSchema } from "../config/db";
+import crypto from 'crypto'
 
-export async function submittrace(id:string , instanceid:string){
+function generateuuid(){
+const uuid = crypto.randomUUID()
+return uuid
+
+}
+
+export async function checkinstance(id:string){
+  const stmt = db.prepare('SELECT * FROM traces where id = ${id}');
+const rows = stmt.columns;
+if(!rows){
+  
+}
+
+}
+
+export async function generateinstance(){
 try{
-
-
+const instanceid = await generateuuid
+return instanceid
 
 }
 catch(err){
-
+console.log("internal server error")
 }
 }
 
